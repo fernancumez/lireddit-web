3a8079fa6bb9a93af5446e82e48db5562650780a
Fernando Cúmez <fernancumez@gmail.com>
2021-06-18 09:54:20 -0600

    Register Error Handling

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,39 +1,31 @@
 import React from 'react';
-import { useMutation } from 'urql';
 import { Form, Formik } from 'formik';
+import { useRouter } from 'next/router';
 import { Box } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
+import { useRegisterUserMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
 
 interface registerProps {}
 
-const REGISTER_MUT = `
-  mutation registerUser($username: String!, $password: String!) {
-    register(options:{ username: $username, password: $password} ) {
-      errors {
-        field
-        message
-      }
-      user{
-        id
-        username
-        createdAt
-        updatedAt
-      }
-    }
-  }
-`;
-
 const Register: React.FC<registerProps> = ({}) => {
-  const [, register] = useMutation(REGISTER_MUT);
+  const router = useRouter();
+  const [, register] = useRegisterUserMutation();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
-          return register(values);
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // worked
+            router.push('/');
+          }
         }}
       >
         {({ isSubmitting }) => (
